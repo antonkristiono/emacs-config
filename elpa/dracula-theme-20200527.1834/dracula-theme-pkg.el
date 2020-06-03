6c03445846afd9d986cb555dd72a926535591e78
Anton Kristiono <antonkristiono47@gmail.com>
2020-06-03 16:31:15 +0700

    first commit

---
+++ elpa/dracula-theme-20200527.1834/dracula-theme-pkg.el
@@ -0,0 +1,10 @@
+(define-package "dracula-theme" "20200527.1834" "Dracula Theme"
+  '((emacs "24.3"))
+  :commit "9705f0136b6c254fb75a39bb0843e0b66b232cff" :authors
+  '(("film42"))
+  :maintainer
+  '("film42")
+  :url "https://github.com/dracula/emacs")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
